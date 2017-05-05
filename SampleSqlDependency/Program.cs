09a24dde400b451aa38f24d1026e60f774a0e4a8
Anupam Shukla <anupam@aspcore.net>
2017-05-05 15:32:34 +0530

    commit 0

---
+++ SampleSqlDependency/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleSqlDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+        public async static void Test()
+        {
+            try
+            {
+                var sqlConnection =
+                    new SqlConnection(
+                        @"Data Source=ANUPAM-PC\MSSQLSERVER2012; Initial Catalog=TestDb; User Id=sa; Password=123");
+                SqlDependency.Start(sqlConnection.ConnectionString);
+                var sqlCommand = new SqlCommand("SELECT * FROM tbl_SysConfig", sqlConnection);
+                sqlConnection.Open();
+                //await sqlCommand.Connection.OpenAsync();
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Notification = null;
+                var sqlDependency = new SqlDependency(sqlCommand);
+                sqlDependency.OnChange += SqlDependencyOnChange;
+                var dr = await sqlCommand.ExecuteReaderAsync();
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private static void SqlDependencyOnChange(object sender, SqlNotificationEventArgs eventArgs)
+        {
+            if (eventArgs.Info == SqlNotificationInfo.Invalid)
+            {
+                Console.WriteLine("The above notification query is not valid.");
+            }
+            else
+            {
+                Console.WriteLine("Notification Info: " + eventArgs.Info);
+                Console.WriteLine("Notification source: " + eventArgs.Source);
+                Console.WriteLine("Notification type: " + eventArgs.Type);
+            }
+        }
+    }
+}
